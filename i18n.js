011c37bb92aa8f2144f18c5d5d253a69fcb0c71e
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-06-08 12:22:10 +0530

    Migrates to Nextjs typescript, adds Rocket.Chat logo

--- i18n.js
+++ i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next'
 import LanguageDetector from "i18next-browser-languagedetector"
 import {initReactI18next} from 'react-i18next'
 import XHR from 'i18next-xhr-backend'
-import languageEN from './locate/en/translate.json'
+import languageEN from './locale/en/translate.json'
 
 i18n
 .use(XHR)
--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-const rcApiDomain = process.env.RC_API_DOMAIN || "http://localhost:3000";
-
-export {
-  rcApiDomain,
-};
