6b81c9d9198ab1c1317c09170a738c4accb70616
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-06-13 09:52:58 +0530

    Moves frontend to /fronted, adds strapi in /backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9e07dfd3396d88a43609466615259199'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9dec839-74e4-4eca-97d8-965fe383865d'
+};
