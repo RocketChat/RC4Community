fad0047c1c71f6c422b9d0057868ba61ab686b39
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-08-06 10:05:29 +0530

    [FIX] Updates database.js to avoid bootstrap function error

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,14 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,
+        pool: {
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
       },
     },
   },
