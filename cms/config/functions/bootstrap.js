13cc006d22c569eaefbe08f6e18039232aaaaa7d
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-08-08 21:49:36 +0530

    [NEW] Automated find permissions on collections for public role on server restart

--- cms/config/functions/bootstrap.js
+++ cms/config/functions/bootstrap.js
@@ -11,8 +11,26 @@
  */
 
 module.exports = async () => {
+
+  const role = await strapi
+    .query("role", "users-permissions")
+    .findOne({ type: "public" });
+  const permissions = await strapi
+    .query("permission", "users-permissions")
+    .find({ type: "application", role: role.id });
+
+  await Promise.all(
+    permissions.map((p) => {
+      if (p.action === "find") {
+        strapi
+          .query("permission", "users-permissions")
+          .update({ id: p.id }, { enabled: true });
+      }
+    })
+  );
+
   // Fetches data and populates CMS from remote on server restart
-  if(process.env.INITIALIZE_DATA) {
+  if (process.env.INITIALIZE_DATA) {
     await strapi.config.functions.fetchData();
   }
 };
