7eac65df9e7f8c5c40c127e9f9e3dbc9b645b234
SingLi <Sing-Li@users.noreply.github.com>
2022-01-10 15:42:06 -0500

    fix breaking strapi from missing env var

--- cms/config/functions/bootstrap.js
+++ cms/config/functions/bootstrap.js
@@ -12,28 +12,34 @@ const { getLatestCommunityActivity } = require("./fetchTopPosts");
  * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#bootstrap
  */
 
-module.exports = async () => {
-
-  const role = await strapi
+ const findPublicRole = async () => {
+  const result = await strapi
     .query("role", "users-permissions")
     .findOne({ type: "public" });
+  return result;
+};
+
+const setDefaultPermissions = async () => {
+  const role = await findPublicRole();
   const permissions = await strapi
     .query("permission", "users-permissions")
     .find({ type: "application", role: role.id });
-
   await Promise.all(
-    permissions.map((p) => {
-      if (p.action === "find") {
-        strapi
-          .query("permission", "users-permissions")
-          .update({ id: p.id }, { enabled: true });
-      }
-    })
+    permissions.map(p =>
+      strapi
+        .query("permission", "users-permissions")
+        .update({ id: p.id }, { enabled: true })
+    )
   );
+};
 
+
+
+module.exports = async () => {
   // Fetches data and populates CMS from remote on server restart
   if (process.env.INITIALIZE_DATA) {
     await strapi.config.functions.fetchData();
     await getLatestCommunityActivity();
+    await setDefaultPermissions();
   }
 };
