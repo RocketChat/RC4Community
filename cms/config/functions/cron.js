c98cfaffac74aab9c2f82f50a710616ce7fff5f1
samad-yar-khan <smdyarkhan123@gmail.com>
2022-03-10 10:09:20 +0530

    Add getIssues Middleware

--- cms/config/functions/cron.js
+++ cms/config/functions/cron.js
@@ -26,7 +26,7 @@ module.exports = {
   '*/60 * * * * *': () => {
     getCommunityContributors('https://gsoc.rocket.chat/api/data','rocketChat','Rocket.Chat');
   },
-  '*/10 * * * * *': () => {
+  '*/120 * * * * *': () => {
     githubKit();
   },
 };
--- cms/config/functions/github.js
+++ cms/config/functions/github.js
@@ -2,120 +2,148 @@ const { Octokit } = require("@octokit/core");
 
 const octokit = new Octokit();
 
-const getRepoData = async function(owner,repo){
+const getRepoData = async function(parent,repo){
 
   try{
       
       let returnedData = await octokit.request("GET /repos/{owner}/{repo}", {
-          owner: owner,
-          repo: repo,
+        owner : parent,
+          repo,
       });
       const {data} = returnedData;
       const {id,name,owner,full_name , html_url , description , stargazers_count,forks_count,open_issues_count,topics } = data;
       const ownerName = owner.login;
       const compactData = {id,full_name , name,ownerName, html_url , description , stargazers_count,forks_count,open_issues_count,topics};
-      return res.json({
+      return {
+          success : true,
           data : compactData
-      })
+      }
 
   }catch(err){
-      return res.json({
+      return {
           error_message : err,
           success:false,
           message:"Internal Server Error!"
-      })
+      }
   }
 
 }
 
+const getIssues = async function(owner,repo){
 
-const getIssues = async (owner, repo) => {
-  try {
-    const res = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-      owner,
-      repo,
-    });
-    let issues = [];
-    if (res.status === 200) {
-      issues = res.data.filter((issue) => !issue.pull_request);
-    }
-    return issues;
-  } catch (err) {
-    return err;
-  }
-};
-
-
-const getContributors = async (owner, repo) => {
-  try {
-    const res = await octokit.request(
-      "GET /repos/{owner}/{repo}/contributors",
-      {
-        owner,
-        repo,
-        per_page: 100,
-      }
-    );
-    let contributors = [];
-    if (res.status === 200) {
-      contributors = res.data;
-    }
-    return contributors;
-  } catch (err) {
-    return err.message;
-  }
-};
-
-module.exports.getGithubIssues = async (owner, repo) => {
-  const Issues = await getIssues(owner, repo);
-  let currentIssues = await strapi.query("ghissue").find();
-    if (currentIssues.length !== 0) {
-      await strapi.query("ghissue").update(
-        { id: currentIssues[0].id },
-        {
-          Issues
-        }
-      );
-    } else {
-      await strapi.query("ghissue").create({
-        Issues
+  try{
+      let returnedData = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+          owner ,
+          repo
+        });
+      let data = returnedData.data;
+      let issueList = [];
+      data.forEach((issue)=>{
+         
+          let newIssue = new Object();
+          if(typeof issue.pull_request === 'undefined' ){
+              newIssue['id'] = issue.id;
+              let {login,avatar_url,html_url} = issue.user;
+              newIssue['user'] = {login,avatar_url,html_url};
+              newIssue['title'] = issue.title;
+              newIssue['number'] = issue.number;
+              newIssue['html_url'] = issue.html_url;
+              issueList.push(newIssue);
+          }
       });
-    }
-};
-
-module.exports.getGithubContributors = async (owner, repo) => {
-  const Contributors = await getContributors(owner, repo);
-  let currentContributors = await strapi.query("ghcontributor").find();
-  if (currentContributors.length !== 0) {
-    await strapi.query("ghcontributor").update(
-      { id: currentContributors[0].id },
-      {
-        Contributors
+      return {
+          success : true ,
+          data : issueList
       }
-    );
-  } else {
-    await strapi.query("ghcontributor").create({
-      Contributors
-    });
+  }catch(err){
+      return {
+          error_message : err,
+          success:false,
+          message:"Internal Server Error!"
+      }
   }
+
 }
 
+const getPulls = async function(owner,repo){
 
-module.exports.githubKit = async () => {
+  try{
+      let returnedData = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
+          owner: owner,
+          repo: repo,
+        });
+      let data = returnedData.data;
+      let pullList = [];
+      data.forEach((pull)=>{
+         
+          let newPull = new Object();
+         
+              newPull['id'] = pull.id;
+              let {login,avatar_url,html_url} = pull.user;
+              newPull['user'] = {login,avatar_url,html_url};
+              newPull['title'] = pull.title;
+              newPull['number'] = pull.number;
+              newPull['state'] = pull.state;
+              // console.log(pull.state);
+              newPull['html_url'] = pull.html_url;
+              pullList.push(newPull);
+        
+      });
+      return {
+          success : true,
+          data : pullList
+      }
+  }catch(err){
+      return {
+          error_message : err,
+          success:false,
+          message:"Internal Server Error!"
+      }
+  }
+
+}
+
+module.exports.githubKit = async function() {
   
   let githubRepositories = await strapi.query('github-repositories').find({});
-  githubKit.forEach(repo => {
+  for(const repo of githubRepositories){
+    // console.log(repo.fresh);
     if(repo.fresh){
-      let repoData = getRepoData(repo.owner,repo.name);
-      repo.fresh = false;
-      repo.repositoryData = repoData;
-      repo.unqiueId;
-      repo.unqiueId = repoData.id;
+      let repoData = await getRepoData(repo.owner,repo.name);
+      // console.log(repoData);
+      if(repoData.success){
+        repo.fresh = false;
+        repo.repositoryData = repoData.data;
+        repo.unqiueId = repoData.data.id;
+      }
+      
+      await strapi.query('github-repositories').update({ 
+        id : repo.id,
+      },repo);
     }
     if(repo.get_issues){
+      let issuesData = await getIssues(repo.owner,repo.name);
+      if(issuesData.success){
+        let issueCount = await strapi.query('ghissue').count({
+          github_repository : repo.id
+        });
+        if(issueCount === 0){
+          let newIssues = await strapi.query('ghissue').create({
+            github_repository : repo.id,
+            Issues : issuesData.data
+          });
+        }else{
+          await strapi.query('ghissue').update({ 
+            github_repository : repo.id,
+          },{
+            github_repository : repo.id,
+            Issues : issuesData.data
+          });
+        }
+      }
 
     }
 
-  });
+  }
 
 }
