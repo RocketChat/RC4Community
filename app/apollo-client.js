2e518579b8affd317109395157c4e17a78ce7e2a
Rohan Lekhwani <rohanlekhwani@ymail.com>
2022-03-20 12:08:40 +0530

    feat: add apollo graphql packages

---
+++ app/apollo-client.js
@@ -0,0 +1,28 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  // Uncomment the appropriate line according to the
+  // region group where you created your database.
+//   uri: 'https://graphql.fauna.com/graphql',
+  // uri: 'https://graphql.eu.fauna.com/graphql',
+  uri: 'https://graphql.us.fauna.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.NEXT_PUBLIC_FAUNA_SECRET
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+export default client;
