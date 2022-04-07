11e9d8f688715fad5fd8b8c80ca2d544ceba893a
samad-yar-khan <smdyarkhan123@gmail.com>
2022-04-08 00:32:02 +0530

    handle edge cases for gitlab and github kit

--- app/components/github/index.js
+++ app/components/github/index.js
@@ -4,11 +4,11 @@ import GithubPullRequestsList from "./githubpullreqeusts";
 import GithubRepo from "./githubrepo";
 
 const Github = (props) => {
-  if(props.type === 'issues'){
+  if(props.type === 'issues' && typeof props.githubData.issues !== "undefined"){
     return (<GithubIssuesList data={props.githubData}/>);
-  }else if(props.type === 'pulls'){
+  }else if(props.type === 'pulls' && typeof props.githubData.pulls !== "undefined"){
     return(<GithubPullRequestsList data={props.githubData}/>)
-  }else if(props.type === 'contributors'){
+  }else if(props.type === 'contributors' && typeof props.githubData.contributors !== "undefined"){
     return (<ContributorsList data={props.githubData}/>);
   }
   return (<GithubRepo data={props.githubData.repositoryData}/>);
--- app/components/gitlab/index.js
+++ app/components/gitlab/index.js
@@ -4,11 +4,11 @@ import GitlabMergeList from "./gitlabMergeRequestList";
 import GitlabProject from "./gitlabProject";
 
 const Gitlab = (props) => {
-  if(props.type === 'issues'){
+  if(props.type === 'issues'&& typeof props.gitlabData.issues !=="undefined"){
     return (<GitlabIssuesList data={props.gitlabData}/>);
-  }else if(props.type === 'merges'){
+  }else if(props.type === 'merges' && typeof props.gitlabData.merges !=="undefined"){
     return(<GitlabMergeList data={props.gitlabData}/>)
-  }else if(props.type === 'members'){
+  }else if(props.type === 'members' && typeof props.gitlabData.members!=="undefined"){
     return (<MembersList data={props.gitlabData}/>);
   }
   return (<GitlabProject data={props.gitlabData.project_data}/>);
