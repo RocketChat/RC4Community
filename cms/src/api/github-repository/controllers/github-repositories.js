9afadc07e9caa94623a3ac01b8c674976831dce1
Dnouv <sdevanshu90@yahoo.com>
2022-05-26 00:00:57 +0530

    Modify ghrepo backend

---
+++ cms/src/api/github-repository/controllers/github-repositories.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  github-repository controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::github-repository.github-repository');
