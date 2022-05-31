753eaec0929ee0230f811575e13d0ff3947a3e9d
Dnouv <sdevanshu90@yahoo.com>
2022-05-31 20:34:54 +0530

    modify populate controller

--- cms/src/api/top-nav-item/controllers/top-nav-item.js
+++ cms/src/api/top-nav-item/controllers/top-nav-item.js
@@ -12,7 +12,13 @@ module.exports = createCoreController('api::top-nav-item.top-nav-item', ({ strap
       // some custom logic here
       
       // Calling the default core action
-      const entity = await strapi.service('api::top-nav-item.top-nav-item').find({populate: true});
+      const entity = await await strapi.entityService.findMany('api::top-nav-item.top-nav-item', {
+        populate: {
+          body: {
+            populate: '*'
+          }
+        },
+      })
       const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
   
       // some more custom logic
