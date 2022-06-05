46e4e250941bacb9b2a3da1f186f8fd08c6ef1ec
Dnouv <sdevanshu90@yahoo.com>
2022-06-05 14:07:57 +0530

    Tidy up

--- app/pages/conferences/confHome.js
+++ app/pages/conferences/confHome.js
@@ -1,12 +1,5 @@
 import Head from "next/head";
 import { Stack } from "react-bootstrap";
-// import { EventHome } from "RCevent";
-import dynamic from 'next/dynamic'
-
-// const {EventHome} = dynamic(() => import('rcevent/home'), {
-//   ssr: false
-// })
-
 
 function EventHomeDemo() {
   return (
@@ -18,11 +11,7 @@ function EventHomeDemo() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Stack className="mx-auto">
-        <h1 className="mx-auto mt-3">Preview of Form Component</h1>
-        <Stack direction="horizontal">
-        </Stack>
-        <Stack direction="horizontal">
-        </Stack>
+        <h1 className="mx-auto mt-3">Preview of Event Component</h1>
         <Stack direction="horizontal">
         </Stack>
       </Stack>
