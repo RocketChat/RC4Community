19eac6591cfe74e3c5e58a62806d04e7a16ed4ba
Dnouv <sdevanshu90@yahoo.com>
2022-06-16 15:13:08 +0530

    Fix complete leaderboard

--- app/lib/leaderboard.js
+++ app/lib/leaderboard.js
@@ -1,21 +1,21 @@
 import { fetchAPI } from "./api";
 
 export const contributorList = (contributors) => {
-    contributors = contributors.sort((contributor1, contributor2) => {
-        if (contributor1.mergedPRsNumber === contributor2.mergedPRsNumber) {
-        if (contributor1.openPRsNumber === contributor2.openPRsNumber) {
-            if (contributor1.issuesNumber < contributor2.issuesNumber) {
+    contributors = contributors.data.sort((contributor1, contributor2) => {
+        if (contributor1.attributes.mergedPRsNumber === contributor2.attributes.mergedPRsNumber) {
+        if (contributor1.attributes.openPRsNumber === contributor2.attributes.openPRsNumber) {
+            if (contributor1.attributes.issuesNumber < contributor2.attributes.issuesNumber) {
             return 1;
             }
             return -1;
         }
-        if (contributor1.openPRsNumber < contributor2.openPRsNumber) {
+        if (contributor1.attributes.openPRsNumber < contributor2.attributes.openPRsNumber) {
             return 1;
         }
         return -1;
         }
 
-        if (contributor1.mergedPRsNumber < contributor2.mergedPRsNumber) {
+        if (contributor1.attributes.mergedPRsNumber < contributor2.attributes.mergedPRsNumber) {
         return 1;
         }
         return -1;
