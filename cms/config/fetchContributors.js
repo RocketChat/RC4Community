13b5926cfb2029af283bf95bc6995f41fa1ffd73
Dnouv <61188295+Dnouv@users.noreply.github.com>
2022-06-17 03:13:08 +0530

    [FIX] Fix leaderboard build fail (#170)
    fix build fail

--- cms/config/fetchContributors.js
+++ cms/config/fetchContributors.js
@@ -55,7 +55,7 @@ module.exports.getCommunityContributors = async (
       if (contributorCount === 0) {
         await strapi
           .service("api::g-so-c-contributor.g-so-c-contributor")
-          .create({ data: { contributor } });
+          .create({ data: contributor });
       } else {
         await strapi
           .db.query("api::g-so-c-contributor.g-so-c-contributor")
@@ -64,7 +64,7 @@ module.exports.getCommunityContributors = async (
               username: contributor.username,
               community: community.id,
             },
-            data: { contributor },
+            data: contributor,
           });
       }
     });
