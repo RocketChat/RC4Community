0e5c313282ca81aa30e07b221476b3935afc2a81
Dnouv <61188295+Dnouv@users.noreply.github.com>
2022-07-17 20:14:47 +0530

    [FIX] Fix strapi top Nav & Remove leaderboard (#177)
    Removed code that blocks all dev and CI.      closes #176

--- cms/src/api/g-so-c-contributor/routes/g-so-c-contributor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * g-so-c-contributor router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::g-so-c-contributor.g-so-c-contributor');
--- cms/src/api/g-so-c-contributor/services/g-so-c-contributor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * g-so-c-contributor service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::g-so-c-contributor.g-so-c-contributor');
