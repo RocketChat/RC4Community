464c6ab85683481b37a013d05675982cd33fca91
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2022-11-21 18:26:57 +0000

    Init container variable after fauna starts

--- startBackend.sh
+++ startBackend.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-FAUNA_CONTAINER_ID=$( docker ps -q -f name=faunadb )
-
 
 echo "--Starting Superprofile Backend--"
 cd superprofile
 sh initFaunaOnce.sh $1
 cd ..
+
+FAUNA_CONTAINER_ID=$( docker ps -q -f name=faunadb )
 
 if [ -z $FAUNA_CONTAINER_ID ]; then
     echo $FAUNA_CONTAINER_ID
