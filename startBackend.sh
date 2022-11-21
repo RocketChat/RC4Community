08e70ac76dda11381c0d85ea0039209077a4ca88
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2022-11-21 18:19:43 +0000

    added after start

--- startBackend.sh
+++ startBackend.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-FAUNA_CONTAINER_ID=$( docker ps -q -f name=faunadb )
-
-
 echo "--Starting Superprofile Backend--"
 cd superprofile
 sh initFaunaOnce.sh $1
 cd ..
 
+FAUNA_CONTAINER_ID=$( docker ps -q -f name=faunadb )
 echo fauna container name $FAUNA_CONTAINER_ID
 
 if [ -z $FAUNA_CONTAINER_ID ]; then
