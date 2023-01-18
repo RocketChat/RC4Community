8c3d863ef02b98635e2e38988fe11ca3f1899ca1
PalanikannanM <palanikannan.m2020@vitstudent.ac.in>
2023-01-18 04:58:05 +0000

    pages directory migrated to app

--- app/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '/styles/globals.css';
-
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <Component {...pageProps} />
-  );
-}
-
-export default MyApp;
